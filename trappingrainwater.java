ed4582cd27fffc1c3899e8d70006a2b0e6d7f784
adhikariabhiram <77536342+adhikariabhiram@users.noreply.github.com>
2022-11-24 13:31:45 +0530

    Update trappingrainwater.java

--- trappingrainwater.java
+++ trappingrainwater.java
@@ -1,4 +1,24 @@
 public class trappingrainwater {
-    
-    
+    public static int trapedwater(int height[]){
+        int leftmax[]=new int[height.length];
+        leftmax[0]=height[0];
+        for(int i=1;i<height.length;i++){
+            leftmax[i]=Math.max(height[i],leftmax[i-1]);
+        }
+        int rightmax[]=new int[height.length];
+        rightmax[height.length]=height[height.length];
+        for(int i=height.length-2;i>=0;i--){
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
+        }
+        int trapedrainwater=0;
+        for(int i=0;i<height.length;i++){
+            int waterlevel=Math.min(leftmax[i],rightmax[i]);
+            trapedrainwater+=waterlevel-height[i];
+        }
+        return trapedrainwater;
+    }
+    public static void main(String[] args) {
+        int height[]={4,2,0,6,3,2,5};
+        System.out.println(trapedwater(height));
+    }
 }
